d3fe4f422f74849e77d9cc7cb91c1037822e077e
mixassio <mixail.bondarev@gmail.com>
2017-10-11 17:54:35 +0300

    working at work

--- budget.py
+++ budget.py
@@ -1,17 +1,35 @@
 import requests
 import json
-page = 8500
-payload = {'pageNum': page}
-url = 'http://www.budget.gov.ru/epbs/registry/ubpandnubp/data'
+import sqlite3
 
+page = 1547
+payload = {'pageNum': page}
+#url = 'http://www.budget.gov.ru/epbs/registry/ubpandnubp/data'
+#url = 'http://budget.gov.ru/epbs/registry/ubpandnubp/data?filterkbk=187'
+url = 'http://budget.gov.ru/epbs/registry/rubp/data?blocks=info'
 response = requests.get(url, params=payload)
 users = response.json()
-
-print(json.dumps(users, indent=2, ensure_ascii=False))
-for i, j in users.items():
-    print(i)
-for i in users['data']:
-    print(json.dumps(i, indent=2, ensure_ascii=False))
-    print('-------------------------------------')
-
-print(type(users['data']), len(users['data']))
+count_page = users['pageCount']
+print(count_page)
+print(users)
+"""
+for page in range(1, count_page - 1):
+    payload = {'pageNum': page}
+    response = requests.get(url, params=payload)
+    users = response.json()
+    my_list = []
+    for i in users['data']:
+        print('-------------------------------------')
+        my_list_val = list(i['info'].values())
+        my_list_key = list(i['info'].keys())
+        print(i['id'])
+        print('-------------------------------------')
+        my_list.append(my_list_val)
+    #my_list.append(my_list_key)
+    conn = sqlite3.connect('./KA.db')
+    c = conn.cursor()
+    for row in my_list:
+        print(row)
+        c.execute("insert into ubp_neubp values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", row)
+    conn.commit()
+    print(page)"""
